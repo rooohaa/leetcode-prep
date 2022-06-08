540f80423c59a9785ab93a5d5454d745c8556c82
Rakhat Alpysbai <rakhatdev@gmail.com>
2022-06-08 23:02:37 +0600

    arrays: maxProfit

--- arrays/maxProfit.js
+++ arrays/maxProfit.js
@@ -1,5 +1,19 @@
+// Time complexity: O(n)
+// Finding optimal solution on each iteration
+
 function maxProfit(prices) {
-  
+  let maxProf = 0;
+  let currentMinPrice = prices[0];
+
+  for (let i = 0; i < prices.length; i++) {
+    let price = prices[i];
+    let profit = price - currentMinPrice;
+
+    maxProf = Math.max(maxProf, profit);
+    currentMinPrice = Math.min(currentMinPrice, price);
+  }
+
+  return maxProf;
 }
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
