34db91acfb84149ebc48fef6e49bf40554ad7fe5
Rakhat Alpysbai <rakhatdev@gmail.com>
2022-07-21 00:02:39 +0600

    arrays: plusOne

---
+++ arrays/plusOne.js
@@ -0,0 +1,17 @@
+function plusOne(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] += 1
+      return digits
+    } else {
+      digits[i] = 0
+    }
+  }
+
+  digits.unshift(1)
+
+  return digits
+}
+
+console.log(plusOne([1, 2, 9]))
+console.log(plusOne([4, 3, 2, 1]))
