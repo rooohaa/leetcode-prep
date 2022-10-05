c519f8b9178be484706b8499e3738dc3fbaa5e68
Rakhat Alpysbai <rakhatdev@gmail.com>
2022-10-05 22:41:14 +0600

    binarySearch: search in rotated sorted array

---
+++ binarySearch/searchRotated.js
@@ -0,0 +1,30 @@
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+
+    if (nums[middle] === target) {
+      return middle;
+    }
+
+    if (nums[left] <= nums[middle]) {
+      if (nums[left] <= target && target <= nums[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    } else {
+      if (nums[middle] <= target && target <= nums[right]) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
