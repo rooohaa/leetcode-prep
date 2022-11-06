530557694f9b90eaa2f3858f492b80263e22a2a3
Rakhat Alpysbai <rakhatdev@gmail.com>
2022-11-06 14:18:40 +0600

    linkedlist: removeNthFromEnd

---
+++ linkedlist/removeNthFromEnd.js
@@ -0,0 +1,31 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0, head);
+  let slow = dummy;
+  let fast = head;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  slow.next = slow.next.next;
+
+  return dummy.next;
+};
+
+const list = new ListNode(1);
+list.next = new ListNode(2);
+list.next.next = new ListNode(3);
+list.next.next.next = new ListNode(4);
+list.next.next.next.next = new ListNode(5);
+
+console.log(removeNthFromEnd(list, 2));
